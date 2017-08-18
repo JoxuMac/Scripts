051cb3f9c13a3a41bc37f3a1af65fd8f3207b599
Josué Gutiérrez <josue.gutierrez1@alu.uclm.es>
2017-08-18 13:27:01 +0200

    Add files via upload
    Subida del Script "Spotify Blocker" que bloquea las direcciones de los servidores de publicidad de la aplicación Spotify en el archivo "hosts" de Windows

---
+++ Spotify Blocker/Spotify Blocker.bat
@@ -0,0 +1,80 @@
+:: SCRIPT SPOTIFY BLOCKER
+:: Actualizado el 13/08/2017
+:: @JoxuMac
+
+:: LISTA DE SERVIDORES BLOQUEADOS
+
+::127.0.0.1 media-match.com
+::127.0.0.1 adclick.g.doublecklick.net
+::127.0.0.1 desktop.spotify.com
+::127.0.0.1 open.spotify.com
+::127.0.0.1 googleads.g.doubleclick.net
+::127.0.0.1 googleadservices.com
+::127.0.0.1 pagead2.googlesyndication.com
+::127.0.0.1 pubads.g.doubleclick.net 
+::127.0.0.1 desktop.spotify.com
+::127.0.0.1 audio2.spotify.com
+::127.0.0.1 pagead46.l.doubleclick.net
+::127.0.0.1 pagead.l.doubleclick.net
+::127.0.0.1 googlehosted.l.googleusercontent.com
+::127.0.0.1 video-ad-stats.googlesyndication.com
+::127.0.0.1 pagead-googlehosted.l.google.com
+::127.0.0.1 partnerad.l.doubleclick.net
+::127.0.0.1 prod.spotify.map.fastlylb.net
+::127.0.0.1 adserver.adtechus.com
+::127.0.0.1 na.gmtdmp.com
+::127.0.0.1 anycast.pixel.adsafeprotected.com
+::127.0.0.1 d361oi6ppvq2ym.cloudfront.net
+::127.0.0.1 gads.pubmatic.com
+::127.0.0.1 idsync-ext.rlcdn.com
+::0.0.0.0 pubads.g.doubleclick.net
+::0.0.0.0 securepubads.g.doubleclick.net
+::0.0.0.0 pubads.g.doubleclick.net
+::0.0.0.0 securepubads.g.doubleclick.net 
+::0.0.0.0 securepubads.g.doubleclick.net
+::0.0.0.0 www.googletagservices.com
+::0.0.0.0 gads.pubmatic.com
+::0.0.0.0 ads.pubmatic.com
+::0.0.0.0 spclient.wg.spotify.com
+
+:: Ruta por defecto C:\Windows\System32\drivers\etc\host
+@echo off
+(
+	echo.
+	echo.
+	echo #SPOTIFY BLOCKER
+	echo.
+	echo 127.0.0.1 media-match.com
+	echo 127.0.0.1 adclick.g.doublecklick.net
+	echo 127.0.0.1 desktop.spotify.com
+	echo 127.0.0.1 open.spotify.com
+	echo 127.0.0.1 googleads.g.doubleclick.net
+	echo 127.0.0.1 googleadservices.com
+	echo 127.0.0.1 pagead2.googlesyndication.com
+	echo 127.0.0.1 pubads.g.doubleclick.net 
+	echo 127.0.0.1 desktop.spotify.com
+	echo 127.0.0.1 audio2.spotify.com
+	echo 127.0.0.1 pagead46.l.doubleclick.net
+	echo 127.0.0.1 pagead.l.doubleclick.net
+	echo 127.0.0.1 googlehosted.l.googleusercontent.com
+	echo 127.0.0.1 video-ad-stats.googlesyndication.com
+	echo 127.0.0.1 pagead-googlehosted.l.google.com
+	echo 127.0.0.1 partnerad.l.doubleclick.net
+	echo 127.0.0.1 prod.spotify.map.fastlylb.net
+	echo 127.0.0.1 adserver.adtechus.com
+	echo 127.0.0.1 na.gmtdmp.com
+	echo 127.0.0.1 anycast.pixel.adsafeprotected.com
+	echo 127.0.0.1 d361oi6ppvq2ym.cloudfront.net
+	echo 127.0.0.1 gads.pubmatic.com
+	echo 127.0.0.1 idsync-ext.rlcdn.com
+	echo 0.0.0.0 pubads.g.doubleclick.net
+	echo 0.0.0.0 securepubads.g.doubleclick.net
+	echo 0.0.0.0 pubads.g.doubleclick.net
+	echo 0.0.0.0 securepubads.g.doubleclick.net 
+	echo 0.0.0.0 securepubads.g.doubleclick.net
+	echo 0.0.0.0 www.googletagservices.com
+	echo 0.0.0.0 gads.pubmatic.com
+	echo 0.0.0.0 ads.pubmatic.com
+	echo 0.0.0.0 spclient.wg.spotify.com
+) >> "C:\Windows\System32\drivers\etc\hosts"
+::) >> "file.txt"
